953d164ac7093daabc91bd8427054e8230c33e8f
ramses2.kouam <ramses2.kouam@abyster.com>
2023-05-06 04:35:33 +0100

    initial commit

--- JEE/script.sh
+++ JEE/script.sh
@@ -3,6 +3,6 @@ $PAYARA_DIR/bin/asadmin --user=admin --passwordfile=/opt/payara/passwordFile sta
     --datasourceclassname com.microsoft.sqlserver.jdbc.SQLServerDataSource \
     --restype javax.sql.DataSource \
     --property User=USER:Password=PASSWORD:DatabaseName=DATABASE_NAME:ServerName=DB_NAME:IntegratedSecurity=false:\
-TrustServerCertificate=false:PortNumber=1433:Encrypt=false ms_Dgirep_Pool && \
+TrustServerCertificate=false:PortNumber=1433:Encrypt=false NOM_POOL_CONNECTION && \
     $PAYARA_DIR/bin/asadmin --user=admin --passwordfile=/opt/payara/passwordFile create-jdbc-resource --connectionpoolid NOM_POOL_CONNECTION NOM_JNDI && \
     $PAYARA_DIR/bin/asadmin --user=admin --passwordfile=/opt/payara/passwordFile deploy $DEPLOY_DIR/NOM_WAR.war && $PAYARA_DIR/bin/asadmin --user=admin --passwordfile=/opt/payara/passwordFile stop-domain production
