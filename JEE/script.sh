410c12a3bd490ac57a8964246b67ce0b1681edad
ramses2.kouam <ramses2.kouam@abyster.com>
2023-05-06 04:34:16 +0100

    second commit

---
+++ JEE/script.sh
@@ -0,0 +1,8 @@
+$PAYARA_DIR/bin/asadmin --user=admin --passwordfile=/opt/payara/passwordFile start-domain production && wait &&\
+    $PAYARA_DIR/bin/asadmin --user=admin --passwordfile=/opt/payara/passwordFile  create-jdbc-connection-pool \
+    --datasourceclassname com.microsoft.sqlserver.jdbc.SQLServerDataSource \
+    --restype javax.sql.DataSource \
+    --property User=USER:Password=PASSWORD:DatabaseName=DATABASE_NAME:ServerName=DB_NAME:IntegratedSecurity=false:\
+TrustServerCertificate=false:PortNumber=1433:Encrypt=false ms_Dgirep_Pool && \
+    $PAYARA_DIR/bin/asadmin --user=admin --passwordfile=/opt/payara/passwordFile create-jdbc-resource --connectionpoolid NOM_POOL_CONNECTION NOM_JNDI && \
+    $PAYARA_DIR/bin/asadmin --user=admin --passwordfile=/opt/payara/passwordFile deploy $DEPLOY_DIR/NOM_WAR.war && $PAYARA_DIR/bin/asadmin --user=admin --passwordfile=/opt/payara/passwordFile stop-domain production
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Dockerfiles_Examples
 Dockerfiles Example for many technologies
+
+Database is SQL SERVER for each dockerfile
